6b2bb5dae5bfbf32039318affcf2a64519fdf41d
baetenJ <jbaeten@ivytech.edu>
2024-02-13 09:07:15 -0500

    Added Pages; Trying to make add course function.

---
+++ script.js
@@ -0,0 +1,11 @@
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("submit", addCourse)
+
+function addCourse(){
+    const courseName = document.getElementById("courseName");
+    const courseCredits = document.getElementById("courseCredits");
+    const courseSubject = document.getElementById("courseSubject");
+    const courseDesc = document.getElementById("courseDesc");
+    const coursesDiv = document.querySelector("#coursesContainer")
+
+}
